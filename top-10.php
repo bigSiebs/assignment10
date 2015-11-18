301855e9745f2399ea7396b26064d30882ffc16b
thechutrain <chu.chuyalan@gmail.com>
2015-11-18 13:49:49 -0500

    added Joe's changes to top-10.php

--- top-10.php
+++ top-10.php
@@ -1,9 +1,6 @@
 <?php
-
 include "top.php";
-
 print "<article>";
-
 // Get activity ID
 if (isset($_POST['btnUpVote']) OR isset($_POST['btnDownVote'])) {
     if (isset($_POST['btnUpVote'])) {
@@ -15,43 +12,46 @@ if (isset($_POST['btnUpVote']) OR isset($_POST['btnDownVote'])) {
     }
     
     print '<section>';
-
     // Query database looking for activity ID
     $checkActivityQuery = "SELECT pmkActivityId";
     $checkActivityQuery .= " FROM tblActivities";
     $checkActivityQuery .= " WHERE pmkActivityId = ?";
     $checkActivityData = array($activityID);
-
     $checkActivity = $thisDatabaseReader->select($checkActivityQuery, $checkActivityData, 1, 0, 0, 0, false, false);
-
-
     // Make sure array returned something; signals that activity ID is valid
     // If invalid, print error
     if (!$checkActivity) {
         print "<p>Invalid activity number.</p>";
     } else { // if valid
+        $selectUserQuery = "SELECT pmkNetId";
+        $selectUserQuery .= " FROM tblAffiliates";
+        $selectUserQuery .= " WHERE pmkNetId = ?";
+        $selectUserData = array($username);
+        $checkUser = $thisDatabaseReader->select($selectUserQuery, $selectUserData, 1, 0, 0, 0, false, false);
+        if (!$checkUser) { // if user is not in affiliates table
+            $userInsertQuery = "INSERT INTO tblAffiliates SET";
+            $userInsertQuery .= " pmkNetId = ?";
+            $userInsertData = array($username);
+            $userInserted = $thisDatabaseWriter->insert($userInsertQuery, $userInsertData, 0, 0, 0, 0, false, false);
+        }
         // Query database for user/activity vote combo
         $checkVoteQuery = "SELECT fldVote";
         $checkVoteQuery .= " FROM tblVotes";
         $checkVoteQuery .= " WHERE fnkActivityId = ? AND";
         $checkVoteQuery .= " fnkNetId = ?";
         $checkVoteData = array($activityID, $username); // username defined in top.php
-
         $checkVote = $thisDatabaseReader->select($checkVoteQuery, $checkVoteData, 1, 1, 0, 0, false, false);
         
         $inserted = "";
         $updated = "";
         
         if (!$checkVote) { // If vote doesn't exist
-
             $insertQuery = "INSERT INTO tblVotes SET";
             $insertQuery .= " fnkNetId = ?,";
             $insertQuery .= " fnkActivityId = ?,";
             $insertQuery .= " fldVote = ?";
             $insertData = array($username, $activityID, $vote);
-
             $inserted = $thisDatabaseWriter->insert($insertQuery, $insertData, 0, 0, 0, 0, false, false);
-
             if ($inserted) {
                 print "<p>Your vote has been tallied. Thanks for voting!</p>";
             }
@@ -71,7 +71,6 @@ if (isset($_POST['btnUpVote']) OR isset($_POST['btnDownVote'])) {
                 $updateQuery .= " WHERE fnkActivityId = ? AND";
                 $updateQuery .= " fnkNetId = ?";
                 $updateData = array($newVote, $activityID, $username);
-
                 $updated = $thisDatabaseWriter->update($updateQuery, $updateData, 1, 1, 0, 0, false, false);
                 
                 if ($updated) {
@@ -81,7 +80,6 @@ if (isset($_POST['btnUpVote']) OR isset($_POST['btnDownVote'])) {
         }
     }
 }
-
 // NEED TO ADD LIMIT CLAUSE
 $query = "SELECT pmkActivityId, fldName, fldOnCampus, fldTownName, fldState";
 $query .= " FROM tblActivities A";
@@ -92,48 +90,37 @@ $query .= " GROUP BY A.fldName";
 $query .= " ORDER BY SUM(fldVote) DESC LIMIT 10";
 $data = array();
 $val = array(1, 1, 0, 0);
-
 // Call select method
 $info = $thisDatabaseReader->select($query, $data, $val[0], $val[1], $val[2], $val[3], false, false);
-
 // To troubleshoot returned array
 if ($debug) {
     print "<p>DATA: <pre>";
     print_r($info);
     print "</pre></p>";
 }
-
 // Start printing table
 print '<table>';
 print '<tr>';
-
 // If user is admin, print blank heading to offset for [Edit] column
 if (adminCheck($username)) {
         print '<th></th>';
     }
-
 // Get headings from first subarray (removes indexes with filter function)
 $fields = array_keys($info[0]);
 $headers = array_filter($fields, 'is_string'); // Picks up only str values
 // Print headings
 foreach ($headers as $head) {
     $camelCase = preg_split('/(?=[A-Z])/', substr($head, 3));
-
     $heading = "";
-
     foreach ($camelCase as $oneWord) {
         $heading .= $oneWord . " ";
     }
-
     print '<th>' . $heading . '</th>';
 }
-
 // Print vote headings for all columns
 print "<th>Vote Up</th>";
 print "<th>Vote Down</th>";
-
 print "</tr>";
-
 // For loop to print records
 foreach ($info as $record) {
     print '<tr>';
@@ -165,7 +152,6 @@ foreach ($info as $record) {
     print 'tabindex="100" class="up-vote">';
     print '</fieldset>';
     print '</form></td>';
-
     // Add downvote form/button
     print '<td>';
     print '<form action="' . $phpSelf . '" method="post" ';
@@ -182,14 +168,10 @@ foreach ($info as $record) {
     print 'tabindex="110" class="down-vote">';
     print '</fieldset>';
     print '</form></td>';
-
     print '</tr>';
 }
-
 // Close table
 print '</table>';
-
 print "</article>";
-
 include "footer.php";
 ?>
