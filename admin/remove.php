4b995cd1159916852ec4da185d723e5db9229452
Joe Siebert <jsiebert@uvm.edu>
2015-12-02 09:58:06 -0500

    Remove.php now displays all data (primary and relational) that will be removed with an activity.

--- admin/remove.php
+++ admin/remove.php
@@ -18,7 +18,9 @@ include "../top.php";
         // if the activity Id is greater than zero, print the activity
         // query database to get all the info on this activity
         // Query of the data given the activity ID
-        $query = "SELECT pmkActivityId, fldName, fldOnCampus, fldTownName, fldState";
+        $query = "SELECT pmkActivityId, fldName, fldCategory, fldOnCampus,";
+        $query .= " fldTownName, fldState, fldLocation, fldCost, fldURL,";
+        $query .= " fldDescription, fnkSubmitNetId, fldDateSubmitted, fldApproved";
         $query .= " FROM tblActivities A";
         $query .= " INNER JOIN tblVotes V ON A.pmkActivityId = V.fnkActivityId";
         $query .= " INNER JOIN tblTowns T ON A.fnkTownId = T.pmkTownId";
@@ -67,61 +69,119 @@ include "../top.php";
             $deleted = false;
         }
     }
-    
+
     if (isset($_POST['btnRemove']) AND $deleted) {
         print "<p>Activity " . $activityID . " and its relational records have been removed.";
-        
     } else if ($activityID == "") { // If activity has not been selected
         print "<p>A valid activity has not been selected. A list of all activities appears below. Please select the item you'd like to remove.</p>";
-        
+
         $query = "SELECT pmkActivityId, fldName";
         $query .= " FROM tblActivities";
         $query .= " ORDER BY fldDateSubmitted";
-        
+
         $selectAll = $thisDatabaseReader->select($query, "", 0, 1, 0, 0, false, false);
-        
+
         print "<section>";
         print '<ul>';
-        
+
         foreach ($selectAll as $record) {
             $appendURL = "?activity=" . $record['pmkActivityId'];
-            
+
             print '<li>';
             print '<a href="' . $appendURL . '">';
             print $record['fldName'];
             print '</a>';
             print '</li>';
         }
-        
+
         print "</section>";
-    
-        
     } else if ($info) { // if valid activity
+        print "<h2>Please confirm you want to remove the activity described below.</h2>";
+        print '<section id="confirm-remove">';
+
+        // Get fld names
+        $fieldKeys = array_keys($info[0]);
+        $fields = array_filter($fieldKeys, 'is_string');
+
+        print '<h3>Name: ' . $info[0]['fldName'] . '</h3>';
+        print '<ol>';
+
+        foreach ($fields as $field) {
+            if ($field != "fldName") {
+                print "<li>";
+
+                $camelCase = preg_split('/(?=[A-Z])/', substr($field, 3));
+
+                $fieldName = "";
+
+                foreach ($camelCase as $oneWord) {
+                    $fieldName .= $oneWord . " ";
+                }
+
+                print '<b>' . $fieldName . ': </b>';
+
+                print $info[0][$field];
+                print "</li>";
+            }
+        }
+
+        print '</ol>';
+
+        print '<p>Removing this record will also remove the following votes.</p>';
+
+        $query = "SELECT fnkNetId, fnkActivityId, fldVote, fldDateVoted";
+        $query .= " FROM tblVotes";
+        $query .= " WHERE fnkActivityId = ?";
+        $query .= " ORDER BY fldDateVoted";
+        $data = array($activityID);
+
+        $info2 = $thisDatabaseReader->select($query, $data, 1, 1, 0, 0, false, false);
+
+        print '<table>';
+
+        // Get fld names
+        $voteKeys = array_keys($info2[0]);
+        $voteFields = array_filter($voteKeys, 'is_string');
+
+        print "<tr>";
+
+        foreach ($voteFields as $field) {
+            print "<th>";
+
+            $camelCase = preg_split('/(?=[A-Z])/', substr($field, 3));
+
+            $fieldName = "";
+
+            foreach ($camelCase as $oneWord) {
+                $fieldName .= $oneWord . " ";
+            }
+
+            print $fieldName;
+            print '</th>';
+        }
+        
+        foreach ($info2 as $record) {
+            print "<tr>";
+            foreach ($voteFields as $field) {
+                print '<td>';
+                print $record[$field];
+                print '</td>';
+            }
+            print "</tr>";
+        }
+
+        print '</table>';
+
+        print '</section>';
         ?>
-        <h2> Please confirm that you want to remove the below entry:</h2>    
-        <table id="confirmRemove">
-            <tr>
-                <td> Activity ID </td>
-                <td> Activity Name </td>
-                <td> Location - City </td>
-                <td> Location - State </td>
-            </tr>
-            <tr>
-                <td> <?php print $info[0]["pmkActivityId"] ?> </td>
-                <td> <?php print $info[0]["fldName"] ?> </td>
-                <td> <?php print $info[0]["fldTownName"] ?> </td>
-                <td> <?php print $info[0]["fldState"] ?> </td>
-            </tr>    
-        </table>
 
         <form action="<?php print $phpSelf; ?>" method="post"
               id="frmRemove">
-            <legend>Please confirm that you would like to remove this activity.</legend>
             <fieldset class="wrapper">        
                 <input type="hidden" id="hidActivityId" name="hidActivityId"
                        value="<?php print $activityID ?>"> 
                 <input type="submit" id="btnRemove" name="btnRemove"
-                       value="Confirm" tabindex="100" class="button">
+                       value="Remove" tabindex="100" class="button">
             </fieldset>
         </form>
         <?php
